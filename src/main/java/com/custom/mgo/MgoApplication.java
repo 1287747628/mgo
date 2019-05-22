419d0e9867b480bafa1ded2df283af68a767d113
1287747628 <1287747628@qq.com>
2019-05-22 22:30:38 +0800

    取消mongodb本地链接配置

--- src/main/java/com/custom/mgo/MgoApplication.java
+++ src/main/java/com/custom/mgo/MgoApplication.java
@@ -2,9 +2,7 @@ package com.custom.mgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
-//@PropertySource(value = {"file:D:\\opt\\fonsview\\NE\\mgo\\etc\\application.properties"})
 @SpringBootApplication
 public class MgoApplication {
 
